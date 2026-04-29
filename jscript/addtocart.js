72e88487a6474ddd8115063dd564da8cf02f4e8c
edu-polmar <polina.martynenko@skola.taby.se>
2026-04-29 12:58:18 +0200

    cart window created and styled

---
+++ jscript/addtocart.js
@@ -0,0 +1,8 @@
+function addToCart(name, price) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push({ name, price });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Product added to cart!");
+}
+
+console.log(localStorage.getItem("cart"));
