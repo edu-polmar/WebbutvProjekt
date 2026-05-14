01e2cbc4218bac697d27ac01a3986ac2a48c9254
edu-polmar <polina.martynenko@skola.taby.se>
2026-05-14 22:24:07 +0200

    footer moved and toastr inplemented

--- jscript/app.js
+++ jscript/app.js
@@ -95,6 +95,24 @@ const addToCart = (product_id) => {
   console.log("Updated carts:", carts);
   addCartToHTML();
   addCartToMemory();
+  toastr["success"]("Item added to Cart!");
+  toastr.options = {
+    closeButton: false,
+    debug: false,
+    newestOnTop: false,
+    progressBar: false,
+    positionClass: "toast-top-right",
+    preventDuplicates: false,
+    onclick: null,
+    showDuration: "300",
+    hideDuration: "1000",
+    timeOut: "5000",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut",
+  };
 };
 
 const addCartToMemory = () => {
